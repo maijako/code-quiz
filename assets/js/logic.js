7d6b2d587ce63bee40f4236a922bcb8fe4809f47
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-15 11:18:43 +0000

    function added to display linked questions and options

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,62 +16,68 @@ startButtonEl.addEventListener("click", beginQuiz);
 
 //Function to begin quiz
 function beginQuiz() {
-    var timeLeft = 75;
-    var timeInterval = setInterval(function () {
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-        if(timeLeft === 0) {
-          clearInterval(timeInterval);
-        }
-    }, 1000);
-    startScreenEl.remove();
-    displayQuestion();
-    displayOptions();
-  }
+  var timeLeft = 75;
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+      if(timeLeft === 0) {
+        clearInterval(timeInterval);
+      }
+      }, 1000);
+  startScreenEl.remove();
+  displayQuestion();
+  displayOptions();
+}
 
-//function to display questions
-  function displayQuestion () {
-    questEl.setAttribute("class", "display");
-    titleEl.textContent = quest1.title; //add a for loop here to loop through the five questions in the questions js, use the questList variable created in questions.js
-  }
+// function to display questions
+function displayQuestion () {
+  questEl.setAttribute("class", "display");
+}
 
-//function to display options within buttons
-  // function displayOptions () {
-  //   var opt1 = document.createElement("button");
-  //   var opt2 = document.createElement("button");
-  //   var opt3 = document.createElement("button");
-  //   var opt4 = document.createElement("button");
-  //   var optItems = [opt1, opt2, opt3, opt4];
-  // for (var i = 0; i < 4; i++) {
-  //   optItems[i].textContent = quest1.choices[i];
-  //   choicesEl.appendChild(optItems[i]);
-  //   };
+//function to display options
+function displayOptions () {
+  var opt1 = document.createElement("button");
+  var opt2 = document.createElement("button");
+  var opt3 = document.createElement("button");
+  var opt4 = document.createElement("button");
+  var optItems = [opt1, opt2, opt3, opt4];
+  for (var i = 0; i < optItems.length; i++){
+    choicesEl.appendChild(optItems[i]);
+    for (var k = 0; k < questList.length; k++) {
+      titleEl.textContent = questList[k].title
+      optItems[i].textContent = questList[k].choices[i];
+     }
+  }
+}
+
+
+
+//function to display options
+// function displayOptions () {
+//   var opt1 = document.createElement("button");
+//   var opt2 = document.createElement("button");
+//   var opt3 = document.createElement("button");
+//   var opt4 = document.createElement("button");
+//   var optItems = [opt1, opt2, opt3, opt4];
+//   for (var i = 0; i < optItems.length; i++){
+//     optItems[i].textContent = questList[i].choices[i];
+//     choicesEl.appendChild(optItems[i])
+//   }
+// }
+    
+  //   function displayQuestion () {
+  //     questEl.setAttribute("class", "display");
+  //     for (var i = 0; i < questList.length; i++){
+  //       titleEl.textContent = questList[i].title;
+  //      }
+  //     }
+  //     optItems.addEventListener("click", displayQuestion, displayOptions);
   // }
- 
-
-  // function displayOptions on click of a button () {
-    //a for loop to display all questions
-    // for (var i = 0; i<questList.length; i++){
-    //   questList[i];
-    var opt1 = document.createElement("button");
-    var opt2 = document.createElement("button");
-    var opt3 = document.createElement("button");
-    var opt4 = document.createElement("button");
-    var optItems = [opt1, opt2, opt3, opt4];
-    for (var i = 0; i < optItems.length; i++){
-      optItems[i].textContent = questList[i].choices[i];
-      console.log(questList[i].choices[i]);
-    }
-    // }
-     
-
 
 // //function to display next question when a button is clicked
 // function nextQuestion() {
 //   optItems.addEventListener("click", displayOptions);
 // }
-
-
 
   // and add logic to change to next question onclick, and to reduce timer by 10 if wrong
   //and if statement or function somewhere to make timer cut by 10 seconds when the wrong option is selected;
