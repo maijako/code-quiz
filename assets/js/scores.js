fc52dd32d21faf7522e0864aaab4211cad111032
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-17 08:03:09 +0000

    edited scoreboard styling

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -15,7 +15,7 @@ highestScores.reverse();
 //A for loop to add a list element for each player with their scores and initials displayed
 for(var i=0; i<highestScores.length; i++){
     var listItem = document.createElement("li");
-    listItem.textContent = highestScores[i].initials+" - "+highestScores[i].score+" 🏆";
+    listItem.textContent = " 🏆 "+highestScores[i].initials+" - "+highestScores[i].score;
     scoresListEl.appendChild(listItem);
 }
 clearButtonEl.addEventListener("click", clearHighScores);
